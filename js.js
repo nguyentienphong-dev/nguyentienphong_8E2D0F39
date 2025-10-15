769e1776b972121a4fbcb88d9a847ee6d2dc1a47
NTPeti <nguyen.tien.phong@ganziskola.hu>
2025-10-15 16:04:51 +0200

    80% kész

--- js.js
+++ js.js
@@ -1,58 +1,96 @@
-
-var rnd = Math.random();
-
+// kész
 function legjobbKivalasztas(){
 
-    
-
+    var ki = document.getElementById("max");
+    maximumHaros = harcosLista[0];
+    for (let i = 0; i < harcosLista.length; i++) {
+        if (harcosLista[i].hp * harcosLista[i].ero > maximumHaros.hp * maximumHaros.ero) {
+            maximumHaros = harcosLista[i];
+        }
+    }
+    ki.textContent = maximumHaros.nev + " a legerősebb " + Math.floor(maximumHaros.ero * maximumHaros.hp) + " ponttal";
 };
 
+// kész
 function legjbenabbKivalasztas(){
-    
-    
+
+    var ki = document.getElementById("min");
+    minimumHaros = harcosLista[0];
+    for (let i = 0; i < harcosLista.length; i++) {
+        if (harcosLista[i].hp * harcosLista[i].ero < minimumHaros.hp * minimumHaros.ero) {
+            minimumHaros = harcosLista[i];
+        }
+    }
+    ki.textContent = minimumHaros.nev + " a leggyengébb " + Math.floor(minimumHaros.ero * minimumHaros.hp) + " ponttal";
 
 };
 
+// kész
 function randomHarcosGenerator(){
-    const nevek1 = ["Sajt", "Gőz", "Acél", "Véres", "Roppantó", "SSR"]
-    const nevek2 = ["Evő", "Fej", "Cérna", "Penge", "SUS", "Ár"]
+    const rnd = Math.random();
+
+    const nevek1 = ["Sajt", "Gőz", "Acél", "Véres", "Roppantó", "SSR"];
+    const nevek2 = ["Evő", "Fej", "Cérna", "Penge", "SUS", "Ár"];
     
-    var nevresz1 = nevek1[Math.round(rnd * nevek1.length)]
-    var nevresz2 = nevek2[Math.round(rnd * nevek2.length)]
-    var nev = nevresz1 + " " + nevresz2;
+    const nevresz1 = nevek1[Math.floor(Math.random() * nevek1.length)];
+    const nevresz2 = nevek2[Math.floor(Math.random() * nevek2.length)];
+    
+    const nev = `${nevresz1} ${nevresz2}`;
+    let hp =  (Math.random() * 9 + 1).toFixed(2);
+    let ero = (Math.random() * 3 + 1).toFixed(2);
 
-    var hp = rnd * 9 + 1;
-    var ero = rnd * 3 + 1;
+    return { nev, hp, ero };
+}
 
-    var harcos = {nev:nev, hp:hp, ero:ero};
-    return harcos;
-};
+// dolgozni kell rajta
+// external js function with dual properties invite from index.html form as action or onclick
+function harc(harcos1Nev, harcos2Nev){
+    var ki = document.getElementById("harcKimenetel");
 
-function harc(harcos1, harcos2){
+    for (let i = 0; i < harcosLista.length; i++) {
+        if (harcosLista[i].nev == harcos1Nev) {
+            harcos1 = harcosLista[i];
+        }
+        if (harcosLista[i].nev == harcos2Nev) {
+            harcos2 = harcosLista[i];
+        }
+    }
+
     while (true){
-        var rnd2 = Math.random();
-        harcos1.hp -= harcos2.ero * rnd2;
-        harcos2.hp -= harcos1.ero * rnd2;
+        var rnd = Math.random();
+        harcos1.hp -= harcos2.ero * rnd;
+        harcos2.hp -= harcos1.ero * rnd;
 
         if (harcos1.hp <= 0 && harcos2.hp <= 0){
-            return "döntetlen";
+            ki.textContent("döntetlen");
         }
         else if (harcos1.hp <= 0){
-            return harcos2.nev + " nyert";
+            ki.textContent(harcos2.nev + " nyert");
         }
         else if (harcos2.hp <= 0){
-            return harcos1.nev + " nyert";
+            ki.textContent(harcos1.nev + " nyert");
         }
     }
 };
 
-var harcosLista = [randomHarcosGenerator ,randomHarcosGenerator, randomHarcosGenerator, randomHarcosGenerator];
-
-function selectionchange(){
-
-    var e = document.getElementById("harcosok2");
-    var harcosCsere = e.harcosLista[e.e1].nev;
-    
-    document.getElementById("").value = harcosCsere;
-    
+// harcosok listája
+var harcosokMennyisege = 4;
+const harcosLista = [];
+for (let i = 0; i < harcosokMennyisege; i++) {
+    harcosLista[i] = randomHarcosGenerator();
 }
+
+
+const select = document.getElementById("selectHarcos1");
+
+for (let i = 0; i < harcosLista.length; i++) {
+    const opt = harcosLista[i];
+
+    const ki = document.createElement("option");
+    ki.textContent = opt.nev;
+    ki.value = opt.nev;
+
+    // valami bajvan az "select.appendChild(ki)"-val!!! 
+    //select.appendChild(ki);
+}
+
